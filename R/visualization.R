b1c5b5915fb0410b3a2918cef39326533623c3f4
Mukai Wang <wangmk@umich.edu>
2025-06-11 13:07:09 -0400

    Add visualization utility functions.

---
+++ R/visualization.R
@@ -0,0 +1,86 @@
+
+
+#' Plot the predicted age against the true age
+#'
+#' @param truth truth vector
+#' @param predicted vector of predicted values
+#' @param diagonal whether to add an identity line, default TRUE
+#' @param title plot title, optional
+#'
+#' @importFrom ggplot2 ggplot aes geom_point scale_x_continuous scale_y_continuous geom_abline ggtitle theme element_text
+#'
+#' @export
+#'
+viz_predict <- function(truth, predicted, diagonal=TRUE, title=NULL){
+
+    predictions_df <- data.frame(Truth=truth, Predicted=predicted)
+    unique_ages <- unique(truth)
+    min_age <- min(truth)
+    max_age <- max(truth)
+
+    myplot <- ggplot(predictions_df, aes(x=.data$Truth, y=.data$Predicted)) +
+        geom_point() + scale_x_continuous(breaks=unique_ages, limits=c(min_age-1, max_age+1))+
+        scale_y_continuous(breaks=unique_ages, limits=c(min_age-1, max_age+1))+
+        theme(text=element_text(size=10))
+
+    if(diagonal){
+        myplot <- myplot + geom_abline(intercept = 0, slope = 1, color = "blue",                # Change line color
+                                      linetype = "dashed",           # Change line style (options include "solid", "dashed", "dotted", etc.)
+                                      size = 0.8)
+    }
+
+    if (!is.null(title)){
+        myplot <- myplot + ggtitle(title)
+    }
+
+    return(myplot)
+
+}
+
+
+
+
+#' Visualize the deconvolution weights
+#'
+#' @param labels labels of training data
+#' @param weights estimated weights
+#' @param truth true value
+#' @param exclude which training sample to remove, optional
+#' @param title title of the ggplot
+#'
+#' @importFrom dplyr group_by %>% arrange summarise
+#' @importFrom ggplot2 ggplot geom_line geom_point scale_x_continuous geom_vline xlab ylab ggtitle theme element_text
+#' @export
+viz_weights <- function(labels, weights, truth, exclude=NULL, title=NULL){
+
+    weights_df <- data.frame(Labels = labels,
+                             Weights= weights) %>%
+        arrange(.data$Labels)
+
+    if (!is.null(exclude)){
+        weights_df <- weights_df[-exclude, ]
+    }
+    weights_df$ID <- seq(1, nrow(weights_df))
+
+    # indices where I assign xticks
+    starting_point <- weights_df %>% group_by(.data$Labels) %>%
+        summarise(index=min(.data$ID))
+
+    # highlight ranges of labels with the same value as truth
+    id_ranges <- weights_df$ID[weights_df$Labels == truth]
+
+    myplot <- ggplot(weights_df, aes(x=.data$ID, y=.data$Weights)) +
+        geom_line(alpha=0.6) + geom_point(size=1.4, alpha=0.6) +
+        scale_x_continuous(breaks=starting_point$index,
+                           labels=as.character(starting_point$Labels)) +
+        geom_vline(xintercept=min(id_ranges)-0.5, linetype="dashed", color="blue")+
+        geom_vline(xintercept=max(id_ranges)+0.5, linetype="dashed", color="blue")+
+        xlab("Age") + ylab("Weights")+
+        theme(text=element_text(size=10))
+
+    if(!is.null(title)){
+        myplot <- myplot + ggtitle(title)
+    }
+    return(myplot)
+
+}
